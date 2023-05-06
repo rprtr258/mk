0a54e7749305c8bda9d993ce6a76cd6ba0d49b0c
Poletansky Viktor <poletansky@gmail.com>
2023-05-06 12:51:48 +0300

    master: split funcs by files

--- mk.go
+++ mk.go
@@ -1,12 +1,7 @@
 package mk
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"io/fs"
-	"os"
-	"os/exec"
 )
 
 type Resource interface {
@@ -155,64 +150,6 @@ func (s System) Build(taskKey string) ([]Resource, error) {
 	return resources, nil
 }
 
-func run(env map[string]string, stdout, stderr io.Writer, cmd string, args ...string) error {
-	c := exec.Command(cmd, args...)
-	c.Env = append(os.Environ(), MapToSlice(env, func(k, v string) string {
-		return k + "=" + v
-	})...)
-	c.Stderr = stderr
-	c.Stdout = stdout
-	c.Stdin = os.Stdin
-
-	if err := c.Run(); err != nil {
-		return fmt.Errorf("cmd %q %v: %w", cmd, args, err)
-	}
-
-	return nil
-}
-
-func ShellCmd(cmd string, args ...string) (stdout string, stderr string, err error) {
-	fmt.Printf("executing %q %v...\n", cmd, args)
-
-	absoluteCmd, err := exec.LookPath(cmd)
-	if err != nil {
-		return "", "", fmt.Errorf("not found %q: %w", cmd, err)
-	}
-
-	stdoutB := bytes.Buffer{}
-	stderrB := bytes.Buffer{}
-	if err := run(nil, &stdoutB, &stderrB, absoluteCmd, args...); err != nil {
-		return "", "", fmt.Errorf(
-			"command failed %q %v stdout=%q stderr=%q: %w",
-			cmd,
-			args,
-			stdoutB.String(),
-			stderrB.String(),
-			err,
-		)
-	}
-
-	return stdoutB.String(), stderrB.String(), nil
-}
-
-func ShellAlias(cmd string, args ...string) func(...string) (stdout string, stderr string, err error) {
-	return func(nargs ...string) (string, string, error) {
-		return ShellCmd(cmd, append(args, nargs...)...)
-	}
-}
-
-func ShellScript(script string) (stdout string, stderr string, err error) {
-	return ShellCmd("/bin/sh", "-c", script)
-}
-
-func MkDir(dir string, perms fs.FileMode) error {
-	if err := os.MkdirAll(dir, perms); err != nil {
-		return fmt.Errorf("mkdir %q with perms=%v: %w", dir, perms, err)
-	}
-
-	return nil
-}
-
 func MapToSlice[K comparable, V, T any](dict map[K]V, f func(K, V) T) []T {
 	res := make([]T, 0, len(dict))
 	for k, v := range dict {
@@ -227,65 +164,4 @@ func Map[T, R any](slice []T, f func(T) R) []R {
 		res[i] = f(elem)
 	}
 	return res
-}
-
-func Must0(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func Must1[A any](a A, err error) A {
-	if err != nil {
-		panic(err)
-	}
-
-	return a
-}
-
-func Must2[A, B any](a A, b B, err error) (A, B) {
-	if err != nil {
-		panic(err)
-	}
-
-	return a, b
-}
-
-func handlePanic(p any) error {
-	if e, ok := p.(error); ok {
-		return e
-	}
-	return fmt.Errorf("panic: %#v", p)
-}
-
-func Try0(f func()) (err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			err = handlePanic(p)
-		}
-	}()
-
-	f()
-	return nil
-}
-
-func Try1[A any](f func() A) (_ A, err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			err = handlePanic(p)
-		}
-	}()
-
-	return f(), nil
-}
-
-func Try2[A, B any](f func() (A, B)) (_ A, _ B, err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			err = handlePanic(p)
-		}
-	}()
-
-	a, b := f()
-	return a, b, nil
 }
---
+++ shell.go
@@ -0,0 +1,68 @@
+package mk
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"os/exec"
+)
+
+func run(env map[string]string, stdout, stderr io.Writer, cmd string, args ...string) error {
+	c := exec.Command(cmd, args...)
+	c.Env = append(os.Environ(), MapToSlice(env, func(k, v string) string {
+		return k + "=" + v
+	})...)
+	c.Stderr = stderr
+	c.Stdout = stdout
+	c.Stdin = os.Stdin
+
+	if err := c.Run(); err != nil {
+		return fmt.Errorf("cmd %q %v: %w", cmd, args, err)
+	}
+
+	return nil
+}
+
+func ShellCmd(cmd string, args ...string) (stdout string, stderr string, err error) {
+	fmt.Printf("executing %q %v...\n", cmd, args)
+
+	absoluteCmd, err := exec.LookPath(cmd)
+	if err != nil {
+		return "", "", fmt.Errorf("not found %q: %w", cmd, err)
+	}
+
+	stdoutB := bytes.Buffer{}
+	stderrB := bytes.Buffer{}
+	if err := run(nil, &stdoutB, &stderrB, absoluteCmd, args...); err != nil {
+		return "", "", fmt.Errorf(
+			"command failed %q %v stdout=%q stderr=%q: %w",
+			cmd,
+			args,
+			stdoutB.String(),
+			stderrB.String(),
+			err,
+		)
+	}
+
+	return stdoutB.String(), stderrB.String(), nil
+}
+
+func ShellAlias(cmd string, args ...string) func(...string) (stdout string, stderr string, err error) {
+	return func(nargs ...string) (string, string, error) {
+		return ShellCmd(cmd, append(args, nargs...)...)
+	}
+}
+
+func ShellScript(script string) (stdout string, stderr string, err error) {
+	return ShellCmd("/bin/sh", "-c", script)
+}
+
+func MkDir(dir string, perms fs.FileMode) error {
+	if err := os.MkdirAll(dir, perms); err != nil {
+		return fmt.Errorf("mkdir %q with perms=%v: %w", dir, perms, err)
+	}
+
+	return nil
+}
