30ebefef4fb92b39ea1cb085cb0d4b1c7195cf3c
Poletansky Viktor <poletansky@gmail.com>
2023-05-19 00:50:37 +0300

    agent: u

--- conc/conc.go
+++ conc/conc.go
@@ -1,4 +1,4 @@
-package mk
+package conc
 
 import "sync"
 
--- idempotent/idempotent.go
+++ idempotent/idempotent.go
@@ -1,6 +1,11 @@
 package idempotent
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rprtr258/mk/conc"
+	"go.uber.org/multierr"
+)
 
 // Action represents action that can be retried several times.
 // If action was alreadey completed, there is no need to run it again.
@@ -11,6 +16,7 @@ type Action interface {
 	Perform() error
 }
 
+// Perform single action. If it is already completed, no action is performed.
 func Perform(action Action) error {
 	completed, errCompleted := action.IsCompleted()
 	if errCompleted != nil {
@@ -24,6 +30,7 @@ func Perform(action Action) error {
 	return action.Perform() //nolint:wrapcheck // return original error
 }
 
+// Multistep - perform idempotent actions by given order.
 func Multistep(actions ...Action) error {
 	for i, action := range actions {
 		if err := Perform(action); err != nil {
@@ -32,4 +39,28 @@ func Multistep(actions ...Action) error {
 	}
 
 	return nil
+}
+
+func Parallel(actions ...Action) error {
+	errch := make(chan error)
+	errs := []error{}
+
+	wg := &conc.WaitGroup{}
+	wg.Go(func() {
+		for err := range errch {
+			errs = append(errs, err)
+		}
+	})
+	for _, action := range actions {
+		action := action
+		wg.Go(func() {
+			if err := Perform(action); err != nil {
+				errch <- err
+			}
+		})
+	}
+	wg.Wait()
+	close(errch)
+
+	return fmt.Errorf("parallel: %w", multierr.Combine(errs...))
 }
